3946dd911ebdde619184d002f907c546aea76aa4
Frista28 <dr.tigh-oleg@yandex.ru>
2026-07-13 00:14:15 +0300

    Add Ship Task 2

---
+++ Assets/20-21/Scripts/Ship.cs
@@ -0,0 +1,76 @@
+using _20_21.Scripts;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private const KeyCode moveLeft = KeyCode.A;
+    private const KeyCode moveRight = KeyCode.D;
+    private const KeyCode sailLeft = KeyCode.Q;
+    private const KeyCode sailRight = KeyCode.E;
+    
+    [SerializeField] private Wind _wind;
+    
+    [SerializeField] private float _rotationSpeed = 5;
+    
+    [SerializeField] private GameObject _sail;
+    
+    private Vector3 _bodyRotation;
+    private Vector3 _sailRotation;
+    
+    // public Vector3 BodyRotation => _bodyRotation;
+    // public Vector3 SailRotation => _sailRotation;
+
+    private void Awake()
+    {
+        _bodyRotation = transform.rotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+        ShipRotate();
+
+        SailRotate();
+        
+        Move();
+    }
+    
+    private void ShipRotate()
+    {
+        float angleDirection = GetAxis(moveLeft, moveRight);
+        
+        float newAngle = _bodyRotation.y + angleDirection * _rotationSpeed * Time.deltaTime;
+        
+        _bodyRotation.y = newAngle;
+        
+        transform.rotation = Quaternion.Euler(_bodyRotation);
+    }
+
+    private void SailRotate()
+    {
+        float angleDirection = GetAxis(sailLeft, sailRight);
+        
+        float newAngle = _sailRotation.y + angleDirection * _rotationSpeed * Time.deltaTime;
+        
+        _sailRotation.y = Mathf.Clamp(newAngle, -90f, 90f);
+        
+        _sail.transform.localRotation = Quaternion.Euler(_sailRotation);
+    }
+    
+    private float GetAxis(KeyCode negative, KeyCode positive)
+    {
+        float value = 0f;
+        if (Input.GetKey(negative)) value -= 1f;
+        if (Input.GetKey(positive)) value += 1f;
+        return value;
+    }
+
+    private void Move()
+    {
+        float dotProduct = Vector3.Dot(_wind.Direction, _sail.transform.forward);
+        
+        if (dotProduct <= 0f)
+            return;
+        
+        transform.position += transform.forward * dotProduct * _wind.Speed * Time.deltaTime;
+    }
+}
---
+++ Assets/20-21/Scripts/Wind.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace _20_21.Scripts
+{
+    public class Wind : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+
+        [SerializeField] private float _timeToChangeDirection;
+        
+        private Vector3 _direction;
+
+        private float _timer;
+        
+        public Vector3 Direction => _direction;
+        public float Speed => _speed;
+
+        private void Awake()
+        {
+            ChangeDirection();
+        }
+
+        private void Update()
+        {
+            if (_timer >= _timeToChangeDirection)
+            {
+                ChangeDirection();
+                _timer = 0;
+            }
+                
+            _timer += Time.deltaTime;
+        }
+
+        private void ChangeDirection()
+        {
+            _direction = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+        }
+    }
+}
