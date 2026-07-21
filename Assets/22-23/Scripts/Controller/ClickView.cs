ea964c5f0e7bdd0c2f52e6eb4489bf5657d7f9d0
Frista28 <dr.tigh-oleg@yandex.ru>
2026-07-21 13:32:25 +0300

    Added background sound and flag class and sound

--- Assets/22-23/Scripts/Controller/ClickView.cs
+++ Assets/22-23/Scripts/Controller/ClickView.cs
@@ -1,39 +1,33 @@
-﻿using UnityEngine;
+﻿using _22_23.Scripts.Items.Marks;
+using UnityEngine;
 
 namespace _22_23.Scripts.Controller
 {
     public class ClickView:  MonoBehaviour
     {
-        [SerializeField] private GameObject _flagPrefab;
+        [SerializeField] private Flag _flagPrefab;
         
         [SerializeField] private ClickFollower _controller;
         
-        private GameObject _flagPrefabInstance;
+        private Flag _flagPrefabInstance;
 
         private void Start()
         {
             _flagPrefabInstance = Instantiate(_flagPrefab, Vector3.zero, Quaternion.identity);
-            _flagPrefabInstance.SetActive(false);
+            _flagPrefabInstance.Disable();
         }
 
         public void Update()
         {
             if (_controller.IsReachedTargetPosition)
             {
-                if (_flagPrefabInstance.activeSelf)
-                    DisableFlag();
+                if (_flagPrefabInstance.IsActive())
+                    _flagPrefabInstance.Disable();
 
                 return;
             }
 
-            _flagPrefabInstance.transform.position = _controller.TargetPosition;
-
-            if (_flagPrefabInstance.activeSelf == false)
-                EnableFlag();
+            _flagPrefabInstance.ChangePosition(_controller.TargetPosition);
         }
-        
-        private void DisableFlag() => _flagPrefabInstance.SetActive(false);
-        
-        private void EnableFlag() => _flagPrefabInstance.SetActive(true);
     }
 }
---
+++ Assets/22-23/Scripts/Items/Marks/Flag.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace _22_23.Scripts.Items.Marks
+{
+    public class Flag : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private AudioClip _audioClip;
+        
+        public bool IsActive() => _meshRenderer.enabled;
+
+        public void ChangePosition(Vector3 position)
+        {
+            if (transform.position != position)
+            {
+                transform.position = position;
+                _audioSource.PlayOneShot(_audioClip);
+            }
+            
+            if(IsActive() == false)
+                _meshRenderer.enabled = true;
+        }
+
+        public void Disable()
+        {
+            _meshRenderer.enabled = false;
+        }
+    }
+}
